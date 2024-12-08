4c5801197a14620ff98ea1d0afd3e25d0bba5014
Triệu Tấn Đạt <dattantrieu20042@gmail.com>
2024-12-08 09:52:32 +0700

    thong bao khi xu li

--- styletransfer.py
+++ styletransfer.py
@@ -9,14 +9,14 @@ import argparse
 
 def style_Transfer(content_dir, style_dir, result_dir):
     models = load_model()
-    
+    i = 1
     content_images = [f for f in os.listdir(content_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
     style_images = [f for f in os.listdir(style_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
     for model_name, model in models.items():
         for content_name in content_images:
             content_path = os.path.join(content_dir, content_name)
             for style_name in style_images:
-                print(f"{content_name} + {style_name}")
+                print(f"{i}) {content_name} + {style_name}")
                 style_path = os.path.join(style_dir, style_name)
 
                 content_image = Image.open(content_path).convert('RGB')
@@ -31,7 +31,8 @@ def style_Transfer(content_dir, style_dir, result_dir):
                 os.makedirs(os.path.dirname(result_path), exist_ok=True)
 
                 output_image.save(result_path, format="PNG")
-                print("Ket qua da duoc luu o", result_path)
+                print("Ket qua da duoc luu")
+                i += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Style Transfer Script")
